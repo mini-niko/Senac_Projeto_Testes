67cf9fde98a1b65bedcf8840f4c16d3c9adc45e0
mini-niko <119255200+mini-niko@users.noreply.github.com>
2023-06-26 20:57:58 -0300

    Primeiro commit
    Primeiro commit

---
+++ senac_testes/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;public class Main {
+    public static void main(String[] args) {
+        int resultado = Calculadora.soma(1, 1);
+
+        System.out.println(resultado);
+    }
+}
---
+++ senac_testes/src/main/test/test/Calculadora/CalculadoraTest.java
@@ -0,0 +1,34 @@
+package test.Calculadora;
+
+import org.example.Calculadora;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @Test
+    void soma() {
+        int resultado = Calculadora.soma(2, 2);
+        Assertions.assertEquals(4, resultado);
+    }
+
+    @Test
+    void subtracao() {
+        int resultado = Calculadora.subtracao(2, 2);
+        Assertions.assertEquals(0, resultado);
+    }
+
+    @Test
+    void multiplicacao() {
+        int resultado = Calculadora.multiplicacao(4, 3);
+        Assertions.assertEquals(12, resultado);
+    }
+
+    @Test
+    void divisao() {
+        int resultado = Calculadora.divisao(9, 3);
+        Assertions.assertEquals(3, resultado);
+    }
+}
